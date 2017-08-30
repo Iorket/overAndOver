944f8444df5d28470057a8477b28d030a595db16
Dmitry <jazscripts@gmail.com>
2017-08-30 12:55:13 +0400

    First Commit

---
+++ PO43/Ulianov/wdad/learn/xml/XmlTask.java
@@ -0,0 +1,18 @@
+package PO43.Ulianov.wdad.learn.xml;
+
+import java.util.List;
+
+public class XmlTask {
+    public List<Reader> negligentReaders(){
+        return  null;
+    }
+    public void removeBook(Reader reader,Book book) {}
+    public void addBook(Reader reader,Book book){}
+    public List<Book> negligentBook(Reader reader){
+        return null;
+    }
+}
+
+class Reader{}
+class Book{}
+
